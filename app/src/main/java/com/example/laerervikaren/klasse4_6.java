0a5986191f6e6d5c3bb91b9b2467705d9886b2f7
ilsoe <ilsoe.tobias@live.dk>
2019-04-10 13:21:05 +0200

    mat activity

--- app/src/main/java/com/example/laerervikaren/klasse4_6.java
+++ app/src/main/java/com/example/laerervikaren/klasse4_6.java
@@ -1,21 +1,43 @@
 package com.example.laerervikaren;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
+import android.widget.Button;
 
 
 public class klasse4_6 extends Fragment {
+
+    private Button button;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_klasse4_6, container, false);
+
+        button = (Button) v.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                OpenMatematik();
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_klasse4_6, container, false);
+
+        return v;
+    }
+
+    public void OpenMatematik(){
+        Intent intent = new Intent(getActivity(), matematik.class);
+        startActivity(intent);
     }
 }
