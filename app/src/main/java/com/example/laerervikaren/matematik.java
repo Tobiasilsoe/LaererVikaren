5531b1f167cdd59d13061f313240437feee7d8a1
ilsoe <ilsoe.tobias@live.dk>
2019-04-10 13:52:16 +0200

    mat activity

---
+++ app/src/main/java/com/example/laerervikaren/matematik.java
@@ -0,0 +1,20 @@
+package com.example.laerervikaren;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class matematik extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matematik);
+    }
+}
